4ab77c4a79f9ee26ead5ec323f08ca0dd4896327
Mohammad Hasani <thebrodmann@protonmail.com>
2020-10-11 15:45:07 +0330

    feat: add victor mono font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,5 @@
+import 'victormono'
+
 export { component as wrapRootElement } from './src/Main'
 export const onInitialClientRender = require('./gatsby/browser/onInitialClientRender').default
 export const onRouteUpdate = require('./gatsby/browser/onRouteUpdate').default
