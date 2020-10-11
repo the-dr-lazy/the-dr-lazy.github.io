d19fb9d248eaade451ce99e56518384804d54100
Mohammad Hasani <thebrodmann@protonmail.com>
2020-10-11 15:32:57 +0330

    fix: remove unused dependencies

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,3 @@
-// Load static fonts
-require('typeface-merriweather')
-// require('source-map-support').install()
-// require('ts-node').register({
-//   compilerOptions: {
-//     module: 'commonjs',
-//     target: 'es2017',
-//   },
-// })
-
 export { component as wrapRootElement } from './src/Main'
 export const onInitialClientRender = require('./gatsby/browser/onInitialClientRender').default
 export const onRouteUpdate = require('./gatsby/browser/onRouteUpdate').default
-// export const shouldUpdateScroll = require('./gatsby/browser/shouldUpdateScroll')
-//   .default
