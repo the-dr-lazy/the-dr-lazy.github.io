eed1c6f64a9f3789efbb0386cf12932ce695bc6a
Mohammad Hasani <the-dr-lazy@pm.me>
2020-12-07 22:26:32 +0330

    style: prettify eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,72 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
-  settings: {
-    react: {
-      version: 'detect',
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+        'import/resolver': {
+            // use <root>/tsconfig.json
+            typescript: {
+                alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+            },
+        },
     },
-    'import/resolver': {
-      // use <root>/tsconfig.json
-      typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-      },
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
     },
-  },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  plugins: ['@typescript-eslint', 'react'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    plugins: ['@typescript-eslint', 'react'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
     },
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-  },
-  rules: {
-    'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
-    'react/display-name': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'import/order': [
-      'error',
-      {
-        groups: ['external', 'builtin', ['sibling', 'parent'], 'index'],
-        pathGroups: [
-          {
-            pattern: '~/**',
-            group: 'builtin',
-          },
+    rules: {
+        'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
+        'react/display-name': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'import/order': [
+            'error',
+            {
+                groups: ['external', 'builtin', ['sibling', 'parent'], 'index'],
+                pathGroups: [
+                    {
+                        pattern: '~/**',
+                        group: 'builtin',
+                    },
+                ],
+                'newlines-between': 'always',
+            },
         ],
-        'newlines-between': 'always',
-      },
-    ],
-    'sort-imports': [
-      'error',
-      {
-        ignoreDeclarationSort: true,
-      },
-    ],
-    'import/no-named-as-default-member': 'off',
-    'import/default': 'off',
-  },
-  overrides: [
-    // Override some TypeScript rules just for .js files
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off', //
-      },
+        'sort-imports': [
+            'error',
+            {
+                ignoreDeclarationSort: true,
+            },
+        ],
+        'import/no-named-as-default-member': 'off',
+        'import/default': 'off',
     },
-  ],
+    overrides: [
+        // Override some TypeScript rules just for .js files
+        {
+            files: ['*.js'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off', //
+            },
+        },
+    ],
 }
